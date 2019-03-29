f84be14ac23bbcc30acc3e7feed3f9bd2fb36010
Sam Buercklin <sam.buercklin@gmail.com>
2019-03-29 15:58:11 -0400

    added letter<->character dictionaries to language help

---
+++ language/language_help.py
@@ -0,0 +1,9 @@
+# Construct two dictionaries: one mapping characters to their number
+#	and one mapping numbers to possible characters
+_ordered_alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+_ordered_numbers = list("22233344455566677778889999")
+
+C_to_N = {k:v for (k,v) in zip(_ordered_alphabet, _ordered_numbers)}
+N_to_C = {k:[] for k in _ordered_numbers}
+for (k,v) in zip(_ordered_numbers, _ordered_alphabet):
+	N_to_C[k] += v
