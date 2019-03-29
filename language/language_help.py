46f85c4d0d550e8c37c5647a1c6f6183652dd7e8
Sam Buercklin <sam.buercklin@gmail.com>
2019-03-29 16:02:04 -0400

    Wrote method in language_help to import English words from .txt file

--- language/language_help.py
+++ language/language_help.py
@@ -7,3 +7,14 @@ C_to_N = {k:v for (k,v) in zip(_ordered_alphabet, _ordered_numbers)}
 N_to_C = {k:[] for k in _ordered_numbers}
 for (k,v) in zip(_ordered_numbers, _ordered_alphabet):
 	N_to_C[k] += v
+
+# Import a text file with a list of words in it, one word
+# per line.
+def import_corpus(file):
+	wordlist = []
+	with open(file) as f:
+		wordlist = f.readlines()
+
+	words = set([w.strip().upper() for w in wordlist])
+
+	return words
