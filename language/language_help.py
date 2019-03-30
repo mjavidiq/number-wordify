715e84bdaa5ea187c12812eb949777b272a28df8
Sam Buercklin <sam.buercklin@gmail.com>
2019-03-30 12:48:09 -0400

    Created corpus class which solves all_wordification; going to refactor on next commit

--- language/language_help.py
+++ language/language_help.py
@@ -20,33 +20,67 @@ def C_to_N(s):
 # 	numbers
 class corpus:
 
-	def __init__(self, fname):
+	def __init__(self, fname, number):
+		self.phone_number = number.replace('-','')
+
 		wordlist = []
 		with open(fname) as f:
 			wordlist = f.readlines()
 		self.words = [w.strip().upper() for w in wordlist]
 
-		# Remove trivial words of individual letters, except for 'A' and 'I' 
-		#	as well as words longer than 10 characters (our maximum length)
-		self.words = list(filter(lambda w: (len(w) > 1) and (len(w) < 11), 
-			self.words) )
+		# Keep only words which could appear SOMEWHERE in the string
+		#	as well as A and I (exclude other letters as "words", e.g. 'F')
+		self.words = list(filter(lambda w: len(w) > 1, self.words))
 		self.words += ['A', 'I']
-		
-		word_nums = set([C_to_N(w) for w in self.words])
-		self.N_to_C_dict = dict.fromkeys(word_nums)
+		self.words = list(
+			filter(lambda w: C_to_N(w) in self.phone_number, self.words) 
+			)
+		self.words = sorted(self.words, key = lambda w: len(w))
+
+		# Pre-allocate the dictionary for speed
+		length = len(number)	
+		dict_nums = set([
+			self.phone_number[i:j+1] for i in xrange(length) for j in xrange(i,length)
+			])
+		self.N_to_C_dict = dict.fromkeys(dict_nums)
 
 		for k in self.N_to_C_dict.keys():
-			self.N_to_C_dict[k] = set([k])
+			self.N_to_C_dict[k] = set([])
 
 		for w in self.words:
 			key = C_to_N(w)
-			self.N_to_C_dict[key].add(w)
+			self.add_number_word(key, w)
+			
 
 	def __call__(self, number):
 		if len(number) == 0: 
 			return set([])
 		
-		if not(number in self.N_to_C_dict.keys()):
-			self.N_to_C_dict[number] = set([number])
+		if self.N_to_C_dict[number] == set([]):
+			return self.add_number_word(number, number)
 
+		return self.N_to_C_dict[number]
+
+	def add_number_word(self, number, word):
+		if len(number) == 0:
+			return set([])
+		self.N_to_C_dict[number].add(word)
+		self.N_to_C_dict[number].add(number)
+		if len(number) > 1:
+			for j in range(len(number)):
+				pivot_char = number[j]
+				lower = self(number[:j])
+				upper = self(number[j+1:])
+				if len(lower) == 0:
+					for suffix in upper:
+						self.N_to_C_dict[number].add(pivot_char + suffix)
+				elif len(upper) == 0:
+					for prefix in lower:
+						self.N_to_C_dict[number].add(prefix + pivot_char)
+				else:
+					for prefix in lower:
+						for suffix in upper:
+							self.N_to_C_dict[number].add(
+								prefix + pivot_char + suffix
+								)
 		return self.N_to_C_dict[number]
