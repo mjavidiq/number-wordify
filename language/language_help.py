66476686e3f912fa97b55afa8b33089600b0896d
Sam Buercklin <sam.buercklin@gmail.com>
2019-03-30 15:40:10 -0400

    updated short language file, added dash fixing

--- language/language_help.py
+++ language/language_help.py
@@ -1,25 +1,47 @@
 import itertools as it
 
 # Construct two dictionaries: one mapping characters to their number
-#	and one mapping numbers to possible characters
+#   and one mapping numbers to possible characters
 _ordered_alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 _ordered_numbers = list("22233344455566677778889999")
 
 _C_to_N = {k:v for (k,v) in zip(_ordered_alphabet, _ordered_numbers)}
 _N_to_C = {k:[] for k in _ordered_numbers}
 for (k,v) in zip(_ordered_numbers, _ordered_alphabet):
-	_N_to_C[k] += v
+    _N_to_C[k] += v
 
 # Map a string of characters to the numbers on the keypad
 def C_to_N(s):
-	for k in _C_to_N.keys():
-		s = s.replace(k, _C_to_N[k])
-	return s
+    for k in _C_to_N.keys():
+        s = s.replace(k, _C_to_N[k])
+    return s
 
+# Given a dictionary file (text file with one word per line)
+#   return the list of words as a list of strings
 def import_dictionary(fname):
-	wordlist = []
-	with open(fname) as f:
-		wordlist = f.readlines()
-	words = [w.rstrip().upper() for w in wordlist]
+    wordlist = []
+    with open(fname) as f:
+        wordlist = f.readlines()
+    words = [w.rstrip().upper() for w in wordlist]
 
-	return words
+    return words
+
+# Characters used for reformatting strings to phone number format
+_relevant_nums = list("0123456789")
+_relevant_chars = _ordered_alphabet + _relevant_nums
+
+# Given a list of wordified numbers, fix each string so that:
+#   1. Each sub-word begins and ends with a single dash
+#   2. The wordified number does not begin or end with a dash '-'
+def fix_dashes(words, number):
+    new_words = []
+    for w in words:
+        new_w = w.replace('--','-')
+        if new_w[0] == '-':
+            new_w = new_w[1::]
+        if new_w[-1] == '-':
+            new_w = new_w[0:-1]
+
+        new_words += [str(new_w)]
+
+    return set(new_words)
