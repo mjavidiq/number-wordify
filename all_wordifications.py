74a39b21447a4fa4050d677b78438b1ca313abdc
Sam Buercklin <sam.buercklin@gmail.com>
2019-03-31 16:03:50 -0400

    added number_to_words function, removed redundant code in all_wordifications

--- all_wordifications.py
+++ all_wordifications.py
@@ -3,7 +3,6 @@ from language.language_help import fix_dashes
 
 def all_wordifications(number, fname = "./language/words_alpha.txt"):
 	worder = wordifier.wordifier(number, fname)
-	wordified = worder(number.replace('-',''))
-	wordified = set([w.replace('--','-') for w in wordified])
+	wordified = set(worder(number.replace('-','')))
 
 	return list(fix_dashes(wordified, number))
---
+++ number_to_words.py
@@ -0,0 +1,14 @@
+import wordifier
+from language.language_help import fix_dashes
+
+def number_to_words(number, fname = "./language/words_alpha.txt"):
+	worder = wordifier.wordifier(number, fname)
+	wordified = set(worder(number.replace('-','')))
+
+	words = list(fix_dashes(wordified, number))
+	
+	retval = words[0]
+	if retval == number and len(words) > 1:
+		retval = words[1]
+
+	return retval
