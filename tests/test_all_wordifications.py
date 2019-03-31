3c1a04b86b1d03d96a5b773e92b7af050fea1e48
Sam Buercklin <sam.buercklin@gmail.com>
2019-03-31 14:45:32 -0400

    added tests to check adjacent words, adding numbers which map to themselves

--- tests/test_all_wordifications.py
+++ tests/test_all_wordifications.py
@@ -22,3 +22,18 @@ def test_small_words():
 		set(["4355","HELL","GELL", "I-355", "HE-55"])]
 	for n, w in zip(test_nums, test_wordifieds):
 		assert set(all_wordifications(n, fname = small_words)) == w		
+
+def test_invariant_numbers():
+    number_pairs = [("1-100-724-6837", "724-6837")]
+    for (n1,n2) in number_pairs:
+    	words1 = set(all_wordifications(n1))
+    	words2 = set(all_wordifications(n2))
+    	assert len(words1) == len(words2)
+    	for w in words2:
+    		assert ("1100" + w in words1) or ("1100-" + w in words1)
+
+def test_adjacent_words():
+	numbers = ["1-100-724-6837","724-6837-1-100",]
+	words = ["1100-SAG-OVER","SAG-OVER-1100"]
+	for (n,w) in zip(numbers, words):
+		assert w in set(all_wordifications(n))
